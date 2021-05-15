4f8f05fc533e2325285731e9cbcb9055e98acaff
xe442 <2496368985@qq.com>
2021-05-15 15:20:47 +0800

    Rewrite symbol table.

--- src/define/symbol_table.cc
+++ src/define/symbol_table.cc
@@ -7,6 +7,8 @@ using namespace compiler::define;
 namespace compiler::define
 {
 
+
+
 /* 
  * default internal functions: 
  * int getint()
@@ -30,66 +32,10 @@ const std::vector<std::pair<std::string, SymbolTableEntry>> SymbolTable::INTERNA
 };
 
 SymbolTable::SymbolTable(std::vector<std::pair<std::string, SymbolTableEntry>> init)
+  : ChainedMap<std::string, SymbolTableEntry>()
 {
-	new_block();
 	for(auto &name_entry_pair : init)
 		_table.back().insert(name_entry_pair);
-}
-
-void SymbolTable::new_block()
-{
-	_table.push_back(std::unordered_map<std::string, SymbolTableEntry>());
-}
-
-void SymbolTable::end_block()
-{
-	if(_table.empty())
-		INTERNAL_ERROR("trying to remove a block from an empty symbol table");
-	_table.pop_back();
-}
-
-std::optional<SymbolTableEntry> SymbolTable::find(const std::string &name) const
-{
-	if(_table.empty())
-		INTERNAL_ERROR("finding an id from an empty symbol table");
-
-	for(auto table_ptr = _table.rbegin(); table_ptr != _table.rend(); ++table_ptr)
-	{
-		auto entry_ptr = table_ptr->find(name);
-		if(entry_ptr != table_ptr->end()) // found
-			return table_ptr->at(name);
-	}
-	return std::nullopt;
-}
-
-bool SymbolTable::insert(const std::string &name, const TypePtr &type)
-{
-	if(_table.empty())
-		INTERNAL_ERROR("inserting an id to an empty symbol table");
-	
-	auto entry_ptr = _table.back().find(name);
-	if(entry_ptr == _table.back().end()) // does not exist, insert it
-	{
-		_table.back()[name] = SymbolTableEntry(type);
-		return true;
-	}
-	else
-		return false;
-}
-
-bool SymbolTable::insert(const std::string &name, const TypePtr &type, int initial_val)
-{
-	if(_table.empty())
-		INTERNAL_ERROR("inserting an id to an empty symbol table");
-	
-	auto entry_ptr = _table.back().find(name);
-	if(entry_ptr == _table.back().end())
-	{
-		_table.back()[name] = SymbolTableEntry(type, initial_val);
-		return true;
-	}
-	else
-		return false;
 }
 
 } // namespace define
--- src/define/symbol_table.h
+++ src/define/symbol_table.h
@@ -2,15 +2,76 @@
 #define SYMBOL_TABLE_H
 
 #include <string>
-#include <vector>
+#include <list>
 #include <unordered_map>
 #include <optional>
-#include "type.h"
 #include "exceptions.h"
+#include "type.h"
 
 
 namespace compiler::define
 {
+
+// Base class of symbol table. Can also be used to hold other chained-block-like elements.
+template<class TKey, class TVal>
+class ChainedMap
+{
+  protected:
+	std::list<std::unordered_map<TKey, TVal>> _table;
+
+  public:
+  	using MapIter = typename std::unordered_map<TKey, TVal>::iterator;
+  	using MapConstIter = typename std::unordered_map<TKey, TVal>::const_iterator;
+  	
+	ChainedMap(): _table() { new_block(); }
+	virtual ~ChainedMap() { end_block(); }
+
+	// Empty is an invalid state for ChainedMap, since the insertion operation
+	// requires at least one block.
+	inline bool empty() const { return _table.empty(); }
+
+	// Adding & removing a block at the end.
+	inline void new_block() { _table.emplace_back(); }
+	inline void end_block()
+		{ if(empty()) INTERNAL_ERROR("no block to end for chained map"); _table.pop_back(); }
+
+	// Searching & inserting operations.
+	std::optional<MapIter> basic_find(const TKey &key)
+	{
+		for(auto riter = _table.rbegin(); riter != _table.rend(); ++riter)
+		{
+			auto find_pos = riter->find(key);
+			if(find_pos != riter->end())
+				return find_pos;
+		}
+		return std::nullopt;
+	}
+	std::optional<MapConstIter> basic_find(const TKey &key) const
+	{
+		for(auto riter = _table.crbegin(); riter != _table.crend(); ++riter)
+		{
+			auto find_pos = riter->find(key);
+			if(find_pos != riter->end()) // found
+				return find_pos;
+		}
+		return std::nullopt;
+	}
+
+	// Returns: the inserted position + whether the insertion is successful.
+	std::pair<MapIter, bool> basic_insert(const std::pair<TKey, TVal> &key_val_pair)
+	{
+		if(empty())
+			INTERNAL_ERROR("cannot insert element to an empty chained map");
+		return _table.back().insert(key_val_pair);
+	}
+
+	std::pair<MapIter, bool> basic_insert(std::pair<TKey, TVal> &&key_val_pair)
+	{
+		if(empty())
+			INTERNAL_ERROR("cannot insert element to an empty chained map");
+		return _table.back().insert(std::move(key_val_pair));
+	}
+};
 
 struct SymbolTableEntry
 {
@@ -26,22 +87,24 @@ struct SymbolTableEntry
 };
 
 // A chained symbol table.
-class SymbolTable
+class SymbolTable: public ChainedMap<std::string, SymbolTableEntry>
 {
-	std::vector<std::unordered_map<std::string, SymbolTableEntry>> _table;
 	static const std::vector<std::pair<std::string, SymbolTableEntry>> INTERNAL_FUNCTIONS;
   
   public:
 	SymbolTable(std::vector<std::pair<std::string, SymbolTableEntry>> init=INTERNAL_FUNCTIONS);
-	~SymbolTable() { end_block(); }
-	inline bool empty() const { return _table.empty(); }
-	void new_block();
-	void end_block();
 
-	std::optional<SymbolTableEntry> find(const std::string &name) const;
-	bool insert(const std::string &name, const TypePtr &type);
-	bool insert(const std::string &name, const TypePtr &type, int initial_val);
-		// return whether the insertion is successful.
+	inline std::optional<SymbolTableEntry> find(const std::string &name) const
+		{
+			auto find_res = basic_find(name);
+			if(!find_res.has_value())
+				return std::nullopt;
+			return find_res.value()->second;
+		}
+	inline bool insert(const std::string &name, const TypePtr &type)
+		{ return basic_insert(std::make_pair(name, SymbolTableEntry(type))).second; }
+	inline bool insert(const std::string &name, const TypePtr &type, int initial_val)
+		{ return basic_insert(std::make_pair(name, SymbolTableEntry(type, initial_val))).second; }
 };
 
 } // namespace compiler::define
